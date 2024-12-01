5f6266d9801048161668a362e45c812e2a60c24b
t632 <tim.kaltenbrunner@zkb.ch>
2024-12-01 10:54:41 +0100

    First commit

---
+++ src/main/java/com/example/demo/AdventOfCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventOfCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdventOfCodeApplication.class, args);
+	}
+
+}
