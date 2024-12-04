18ac3cb31ce36f885f55dd2e145321144cd03375
t632 <tim.kaltenbrunner@zkb.ch>
2024-12-04 08:57:12 +0100

    Day 4: Again with java

--- src/main/java/ch/zkb/t632/java/Day4.java
+++ src/main/java/ch/zkb/t632/java/Day4.java
@@ -3,7 +3,7 @@ package ch.zkb.t632.java;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.regex.Pattern;
+import java.util.List;
 
 import jakarta.annotation.PostConstruct;
 
@@ -16,34 +16,100 @@ public class Day4 {
   @Value("input/puzzle4_test.csv")
   private Path puzzle4_test;
 
+  @Value("input/puzzle4_test2.csv")
+  private Path puzzle4_test2;
+
   @Value("input/puzzle4.csv")
   private Path puzzle4;
 
+  private static final String XMAS = "XMAS";
+
+  private static final String XMAS_REV = "SAMX";
+
+  private static final String MAS = "MAS";
+
+  private static final String MAS_REV = "SAM";
+
   @PostConstruct
   public void solve() throws IOException {
-    System.out.println("Test:   " + task(puzzle4_test));
-    System.out.println("Result: " + task(puzzle4));
+    System.out.println("Test 1:   " + task1(puzzle4_test));
+    System.out.println("Result 1: " + task1(puzzle4));
+    System.out.println("Test 2:   " + task2(puzzle4_test));
+    System.out.println("Result 1: " + task2(puzzle4));
   }
 
-  public long task(Path path) throws IOException {
+  public long task1(Path path) throws IOException {
     var lines = Files.readAllLines(path);
-    var input = String.join("", lines);
-    var splitted = input.split("don't\\(\\)");
-    var edited = splitted[0];
-    for (int i = 1; i < splitted.length; i++) {
-      var index = splitted[i].indexOf("do()");
-      if (index > -1) {
-        edited += splitted[i].substring(index);
-      }
-    }
-    var pattern = Pattern.compile("mul\\((\\d{0,3}),(\\d{0,3})\\)");
-    var matcher = pattern.matcher(edited);
-    long result = 0;
-    while (matcher.find()) {
-      result += Long.parseLong(matcher.group(1)) * Long.parseLong(matcher.group(2));
-    }
-
+    var result = findHorizontally(lines);
+    result += findVertically(lines);
+    result += findOther(lines);
     return result;
   }
 
+  public long task2(Path path) throws IOException {
+    var lines = Files.readAllLines(path);
+    return findXMAS(lines);
+  }
+
+  private int findHorizontally(List<String> lines) {
+    var matches = 0;
+    for (String line : lines) {
+      for (int l = 0; l < line.length() - 3; l++) {
+        var sub = line.substring(l, l + 4);
+        if (sub.equals(XMAS) || sub.equals(XMAS_REV)) {
+          matches++;
+        }
+      }
+    }
+    return matches;
+  }
+
+  private int findVertically(List<String> lines) {
+    var matches = 0;
+    for (int i = 0; i < lines.size() - 3; i++) {
+      for (int l = 0; l < lines.getFirst().length(); l++) {
+        var sub = "" + lines.get(i).charAt(l) + lines.get(i + 1).charAt(l) + lines.get(i + 2).charAt(l) + lines.get(i + 3).charAt(l);
+        if (sub.equals(XMAS) || sub.equals(XMAS_REV)) {
+          matches++;
+        }
+      }
+    }
+    return matches;
+  }
+
+  private int findOther(List<String> lines) {
+    var matches = 0;
+    for (int i = 0; i < lines.size() - 3; i++) {
+      for (int l = 0; l < lines.getFirst().length() - 3; l++) {
+        var sub = "" + lines.get(i).charAt(l) + lines.get(i + 1).charAt(l + 1) + lines.get(i + 2).charAt(l + 2) + lines.get(i + 3).charAt(l + 3);
+        if (sub.equals(XMAS) || sub.equals(XMAS_REV)) {
+          matches++;
+        }
+      }
+    }
+
+    for (int i = 0; i < lines.size() - 3; i++) {
+      for (int l = 3; l < lines.getFirst().length(); l++) {
+        var sub = "" + lines.get(i).charAt(l) + lines.get(i + 1).charAt(l - 1) + lines.get(i + 2).charAt(l - 2) + lines.get(i + 3).charAt(l - 3);
+        if (sub.equals(XMAS) || sub.equals(XMAS_REV)) {
+          matches++;
+        }
+      }
+    }
+    return matches;
+  }
+
+  private int findXMAS(List<String> lines) {
+    var matches = 0;
+    for (int i = 0; i < lines.size() - 2; i++) {
+      for (int l = 0; l < lines.getFirst().length() - 2; l++) {
+        var sub1 = "" + lines.get(i).charAt(l) + lines.get(i + 1).charAt(l + 1) + lines.get(i + 2).charAt(l + 2);
+        var sub2 = "" + lines.get(i + 2).charAt(l) + lines.get(i + 1).charAt(l + 1) + lines.get(i).charAt(l + 2);
+        if ((sub1.equals(MAS) || sub1.equals(MAS_REV)) && (sub2.equals(MAS) || sub2.equals(MAS_REV))) {
+          matches++;
+        }
+      }
+    }
+    return matches;
+  }
 }
--- src/main/java/ch/zkb/t632/kotlin/year24/day04/Day04.kt
+++ src/main/java/ch/zkb/t632/kotlin/year24/day04/Day04.kt
@@ -7,36 +7,13 @@ fun main() {
     val testInput1 = readInput("2024", "Day04_test_part1")
     check(part1(testInput1), 161)
 
-
-    // Change to 04
-    val input = readInput("2024", "Day03")
+    val input = readInput("2024", "Day04")
     println(part1(input))
 }
 
-private fun part1(input: List<String>): Int = input.getCommands().filterIsInstance<Mul>().sumOf { it.product }
+private fun part1(input: List<String>): Int = input.search()
 
-private sealed interface Command
-private data object Do : Command
-private data object Dont : Command
-private data class Mul(
-    val a: Int,
-    val b: Int,
-) : Command {
-    val product = a * b
-}
 
-private val pattern = "(do\\(\\)|don't\\(\\)|mul\\(\\d+,\\d+\\))".toRegex()
-
-private fun List<String>.getCommands(): List<Command> {
-    return pattern.findAll(joinToString("")).map {
-        val text = it.value
-        if (text.startsWith("mul")) {
-            val (a, b) = text.removePrefix("mul(").removeSuffix(")").split(",")
-            Mul(a.toInt(), b.toInt())
-        } else if (text == "do()") {
-            Do
-        } else {
-            Dont
-        }
-    }.toList()
+private fun List<String>.search(): Int {
+    return 0;
 }
