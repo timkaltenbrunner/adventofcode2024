ed662f7a954906bfbb1caf72aff86de734a8a8e4
Tim Kaltenbrunner <tim.kaltenbrunner@gmail.com>
2024-12-09 10:04:11 +0100

    Day 9: Part 2

--- src/main/java/ch/zkb/t632/kotlin/year24/day09/Day09.kt
+++ src/main/java/ch/zkb/t632/kotlin/year24/day09/Day09.kt
@@ -5,25 +5,17 @@ import ch.zkb.t632.kotlin.readInput
 
 fun main() {
     val testInput = readInput("2024", "Day09_test")
-    check(part1(testInput), 1928)
+    check(part2(testInput), 2858)
 
     val input = readInput("2024", "Day09")
-    val resultPart1 = part1(input)
+    val resultPart1 = part2(input)
 
-    println("Solution of Part1: $resultPart1")
+    println("Solution of Part2: $resultPart1")
 
-
-    /*
-    check(part2(testInput), 34)
-    val resultPart2 = part2(input)
-
-    println("Solution of Part2: $resultPart2")
-     */
 }
 
-private fun part1(input: List<String>): Long = solve(parseInputs(input.first()))
+private fun part2(input: List<String>): Long = solve(parseInputs(input.first()))
 
-private fun part2(input: List<String>): Long = 0
 
 private fun solve(parsedInputs: List<Long>): Long =
     multiply(compact(parsedInputs).print()).print().map { it.toLong() }.sum()
@@ -34,21 +26,53 @@ private fun List<Long>.print(): List<Long> {
 }
 
 
-private fun compact(input: List<Long>): List<Long> = buildList {
+private fun compact(input: List<Long>): List<Long> {
     var revIndex = input.size - 1
-    for ((index, value) in input.withIndex()) {
-        if (value == -1L) {
-            var revVal = input[revIndex--]
-            while (revVal == -1L) {
-                if (index >= revIndex) return@buildList
-                revVal = input[revIndex--]
+    val copy = input.toMutableList()
+    do {
+        val (localRevVal, revSize, localRevIndex) = getRevBlock(copy, revIndex)
+        revIndex = localRevIndex
+        var index = 0
+        while (index < revIndex) {
+            val value = copy[index]
+            if (value == -1L) {
+                val emptyBlockSize = calcEmptyBlockSize(copy, index)
+                index += emptyBlockSize
+                if (emptyBlockSize >= revSize) {
+                    updateNumTimes(copy, revSize, revIndex + 1, 0)
+                    updateNumTimes(copy, revSize, index - emptyBlockSize, localRevVal)
+                    break
+                }
+
             }
-            add(revVal)
-        } else {
-            add(value)
+            index++
         }
-        if (index >= revIndex) break
+    } while (revIndex > 0)
+    return copy.map { if (it < 0) 0 else it }.toList()
+}
+
+
+fun calcEmptyBlockSize(longs: MutableList<Long>, index: Int): Int {
+    var size = 0
+    while (index + ++size in longs.indices && longs.get(index + size) == -1L) {
     }
+    return size
+}
+
+private fun getRevBlock(copy: MutableList<Long>, revIndex: Int): Triple<Long, Int, Int> {
+    var localRevSize = 0
+    var localRevVal = copy[revIndex]
+    var localRevIndex = revIndex
+    while (localRevVal == -1L && localRevIndex - 1 in copy.indices) {
+        localRevVal = copy[--localRevIndex]
+    }
+    var tempRevValue = localRevVal
+    while (localRevVal == tempRevValue && localRevIndex - 1 in copy.indices) {
+        localRevSize++
+        tempRevValue = copy[--localRevIndex]
+    }
+    return Triple(localRevVal, localRevSize, localRevIndex)
+
 }
 
 private fun multiply(input: List<Long>): List<Long> = buildList {
@@ -67,6 +91,12 @@ private fun parseInputs(input: String): List<Long> = buildList {
 fun addNumTimes(num: Long, times: Int): List<Long> = buildList {
     for (j in 1..times) {
         add(num)
+    }
+}
+
+fun updateNumTimes(copy: MutableList<Long>, revSize: Int, index: Int, value: Long) {
+    for (j in index..<(index + revSize)) {
+        copy[j] = value
     }
 }
 
