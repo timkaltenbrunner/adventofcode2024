be148dcc19fe8e76214abb58e77e5a98dbf5b125
Tim Kaltenbrunner <tim.kaltenbrunner@gmail.com>
2024-12-11 06:36:08 +0100

    Day 11: Part 1

--- src/main/java/ch/zkb/t632/kotlin/year24/day11/Day11.kt
+++ src/main/java/ch/zkb/t632/kotlin/year24/day11/Day11.kt
@@ -5,47 +5,53 @@ import ch.zkb.t632.kotlin.readInput
 
 fun main() {
     val testInput = readInput("2024", "Day11_test")
-    check(part1(testInput), 2858)
+    check(part1(testInput, 25), 55312)
 
     val input = readInput("2024", "Day11")
-    val resultPart1 = part1(input)
+    val resultPart1 = part1(input, 25)
 
     println("Solution of Part1: $resultPart1")
 
 }
 
-private fun part1(input: List<String>): Long = input.parseInputs().solve()
+private fun part1(input: List<String>, blinkTimes: Int): Int = input.parseInputs().solve(blinkTimes)
 
 private data class Position(val height: Int, var reachablePos9: Set<Pair<Int, Int>>, var reachablePos1: Int)
 
 
-private fun List<List<Position>>.solve(): Long {
-    var solution = 0L
-    for (cur in 9 downTo 0) {
-        for ((y, row) in this.withIndex()) {
-            for ((x, pos) in row.withIndex()) {
-                if (pos.height == cur) {
-                    solution++
+private fun List<Long>.solve(blinkTimes: Int): Int {
+    var current = this
+    repeat(blinkTimes) {
+        // println(current)
+        current = buildList {
+            for (stone in current) {
+                if (stone == 0L) {
+                    add(1L)
+                } else if (("" + stone).length % 2 == 0) {
+                    val (s1, s2) = stone.split()
+                    add(s1)
+                    add(s2)
+                } else {
+                    add(stone * 2024)
                 }
             }
         }
     }
-    return solution
+    return current.count()
+}
+
+private fun Long.split(): Pair<Long, Long> {
+    val stone = "" + this
+    return stone.substring(0, stone.length / 2).toLong() to stone.substring(stone.length / 2, stone.length).toLong()
 }
 
 
-private fun List<String>.parseInputs(): List<List<Position>> {
+private fun List<String>.parseInputs(): List<Long> {
     val input = this
     return buildList {
-        for ((y, row) in input.withIndex()) {
-            add(buildList {
-                for ((x, ch) in row.withIndex()) {
-                    val height = ("" + ch).toInt()
-                    val reachablePos9 = if (height == 9) setOf(Pair(y, x)) else setOf()
-                    val reachablePos1 = if (height == 9) 1 else 0
-                    add(Position(height, reachablePos9, reachablePos1))
-                }
-            })
+        val row = input.first()
+        for (num in row.split(" ")) {
+            add(num.toLong())
         }
     }
 }
