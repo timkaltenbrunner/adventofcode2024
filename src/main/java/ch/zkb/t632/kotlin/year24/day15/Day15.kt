d513bb1647d1524fe142dd992bcd43938d3dfe06
Tim Kaltenbrunner <tim.kaltenbrunner@gmail.com>
2024-12-15 09:47:33 +0100

    Day 15: Part 2

--- src/main/java/ch/zkb/t632/kotlin/year24/day15/Day15.kt
+++ src/main/java/ch/zkb/t632/kotlin/year24/day15/Day15.kt
@@ -5,12 +5,12 @@ import ch.zkb.t632.kotlin.readInput
 
 fun main() {
     val testInput = readInput("2024", "Day15_test")
-    check(solve(testInput), 10092)
+    check(solve(testInput), 9021)
 
     val input = readInput("2024", "Day15")
     val resultPart1 = solve(input)
 
-    println("Solution of Part1: $resultPart1")
+    println("Solution of Part2: $resultPart1")
 
 }
 
@@ -20,12 +20,36 @@ private fun solve(input: List<String>): Long = input.parseInputs().solve()
 private data class Warehouse(val map: Map, val movements: List<Pos>) {
     fun canMove(movement: Pos): Boolean {
         var pos = map.robot
-        do {
-            pos = pos + movement
-            if (map.walls.contains(pos)) {
+        pos += movement
+        if (map.walls.contains(pos)) {
+            return false
+        }
+        val box = map.boxes.findBox(pos)
+        if (box != null) {
+            return canMove(box, movement)
+        }
+        return true
+    }
+
+    private fun canMove(box: Box, movement: Pos, boxes: MutableSet<Box> = mutableSetOf()): Boolean {
+        val pos1 = box.start + movement
+        val pos2 = box.end + movement
+        if (map.walls.contains(pos1)) return false
+        if (map.walls.contains(pos2)) return false
+        val box1 = map.boxes.findBox(pos1)
+        val box2 = map.boxes.findBox(pos2)
+        if (box1 != null && !boxes.contains(box1)) {
+            boxes.add(box1)
+            if (!canMove(box1, movement, boxes)) {
                 return false
             }
-        } while (map.boxes.contains(pos))
+        }
+        if (box2 != null && !boxes.contains(box2)) {
+            boxes.add(box2)
+            if (!canMove(box2, movement, boxes)) {
+                return false
+            }
+        }
         return true
     }
 
@@ -35,22 +59,51 @@ private data class Warehouse(val map: Map, val movements: List<Pos>) {
     }
 
     private fun shiftBoxes(pos: Pos, movement: Pos) {
-        if (pos in map.boxes) {
-            var next = pos
-            while (next in map.boxes) {
-                next += movement
-            }
-            map.boxes.remove(pos)
-            map.boxes.add(next)
+        val box = map.boxes.findBox(pos)
+        if (box != null) {
+            val boxes = findBoxes(box, movement)
+            map.boxes.removeAll(boxes)
+            val movedBoxes = boxes.map { Box(it.start + movement, it.end + movement) }
+            map.boxes.addAll(movedBoxes)
         }
+    }
+
+    private fun findBoxes(box: Box, movement: Pos, boxes: MutableSet<Box> = mutableSetOf()): Set<Box> {
+        boxes += box
+        val pos1 = box.start + movement
+        val pos2 = box.end + movement
+        val box1 = map.boxes.findBox(pos1)
+        val box2 = map.boxes.findBox(pos2)
+        if (box1 != null && !boxes.contains(box1)) {
+            findBoxes(box1, movement, boxes)
+        }
+        if (box2 != null && !boxes.contains(box2)) {
+            findBoxes(box2, movement, boxes)
+        }
+        return boxes
     }
 }
 
-private data class Map(var robot: Pos, val boxes: MutableSet<Pos>, val walls: Set<Pos>, val maxX: Int, val maxY: Int)
+private data class Map(var robot: Pos, val boxes: MutableSet<Box>, val walls: Set<Pos>)
+
+private data class Box(val start: Pos, val end: Pos)
 
 private data class Pos(val x: Int, val y: Int) {
     operator fun plus(other: Pos): Pos = Pos(x + other.x, y + other.y)
 }
+
+private fun Set<Box>.findBox(pos: Pos): Box? {
+    val right = Box(pos, Pos(pos.x + 1, pos.y))
+    if (contains(right)) {
+        return right
+    }
+    val left = Box(Pos(pos.x - 1, pos.y), pos)
+    if (contains(left)) {
+        return left
+    }
+    return null
+}
+
 
 private fun Warehouse.solve(): Long {
     for (movement in movements) {
@@ -58,37 +111,33 @@ private fun Warehouse.solve(): Long {
             move(movement)
         }
     }
-    return map.boxes.sumOf { (x, y) -> 100L * y + x }
+    return map.boxes.sumOf { (start, _) -> 100L * start.y + start.x }
 }
 
 
 private fun List<String>.parseInputs(): Warehouse {
     var robot = Pos(0, 0)
-    val boxes: MutableSet<Pos> = mutableSetOf()
+    val boxes: MutableSet<Box> = mutableSetOf()
     val walls: MutableSet<Pos> = mutableSetOf()
     val movements: MutableList<Pos> = mutableListOf()
-    var maxX = 0
-    var maxY = 0
     val input = this
     for ((y, row) in input.withIndex()) {
         for ((x, ch) in row.withIndex()) {
             when (ch) {
+                '@' -> robot = Pos(x * 2, y)
                 '#' -> {
-                    maxX = kotlin.math.max(maxX, x)
-                    maxY = kotlin.math.max(maxY, y)
-                    walls.add(Pos(x, y))
+                    walls.add(Pos(x * 2, y))
+                    walls.add(Pos(x * 2 + 1, y))
                 }
-
-                'O' -> boxes.add(Pos(x, y))
+                'O' -> boxes.add(Box(Pos(x * 2, y), Pos(x * 2 + 1, y)))
                 '>' -> movements.add(Pos(1, 0))
                 '<' -> movements.add(Pos(-1, 0))
                 '^' -> movements.add(Pos(0, -1))
                 'v' -> movements.add(Pos(0, 1))
-                '@' -> robot = Pos(x, y)
             }
         }
     }
-    return Warehouse(Map(robot, boxes, walls, maxX, maxY), movements)
+    return Warehouse(Map(robot, boxes, walls), movements)
 }
 
 
