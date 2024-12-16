6098c6a2cbc8e9e269bb3fa6be198a94db6aa03e
Tim Kaltenbrunner <tim.kaltenbrunner@gmail.com>
2024-12-16 09:57:02 +0100

    Day 16: Part 1 with AStar search

--- src/main/java/ch/zkb/t632/kotlin/year24/day16/Day16.kt
+++ src/main/java/ch/zkb/t632/kotlin/year24/day16/Day16.kt
@@ -2,103 +2,92 @@ package ch.zkb.t632.kotlin.year24.day16
 
 import ch.zkb.t632.kotlin.check
 import ch.zkb.t632.kotlin.readInput
+import kotlin.math.abs
 
 fun main() {
     val testInput = readInput("2024", "Day16_test")
-    check(part1(testInput), 1930)
+    check(part1(testInput), 11048)
 
     val input = readInput("2024", "Day16")
     val resultPart1 = part1(input)
     println("Solution of Part1: $resultPart1")
 }
 
-private fun part1(input: List<String>): Long = input.parseInputs().solve().sumOf { 1L }
+private fun part1(input: List<String>): Int = input.parseInputs().solve()
 
-private fun List<List<Area>>.solve(): Set<Garden> {
-    val foundAreas = mutableSetOf<Area>()
-    val gardens = mutableSetOf<Garden>()
-    for (row in this) {
-        for (area in row) {
-            if (!foundAreas.contains(area)) {
-                val garden = Garden(area.type, mutableSetOf(area))
-                findAllNeighbours(area, garden)
-                foundAreas += garden.areas
-                gardens += garden
+private fun Map.solve(): Int {
+    val node = aStar(
+        from = start,
+        goal = ::isEnd,
+        neighboursWithCost = ::neighboursWithCost,
+        heuristic = ::heuristic
+    )
+    if (node == null) {
+        return -1
+    }
+    return node.cost
+}
+
+private data class Pos(val x: Int, val y: Int) {
+    operator fun plus(other: Pos): Pos = Pos(x + other.x, y + other.y)
+}
+
+private data class PosDir(val pos: Pos, val direction: Direction) {
+    fun rotationCosts(end: Pos): Int = when (direction) {
+        Direction.UP -> if (pos.y < end.y) 2000 else if (pos.x != end.x) 1000 else 0
+        Direction.DOWN -> if (pos.y > end.y) 2000 else if (pos.x != end.x) 1000 else 0
+        Direction.RIGHT -> if (pos.x > end.x) 2000 else if (pos.y != end.y) 1000 else 0
+        Direction.LEFT -> if (pos.x < end.x) 2000 else if (pos.y != end.y) 1000 else 0
+    }
+}
+
+private data class Map(var start: PosDir, var end: Pos, val walls: Set<Pos>) {
+    fun isEnd(posDir: PosDir): Boolean = posDir.pos == end
+    fun heuristic(posDir: PosDir): Int =
+        abs(end.x - posDir.pos.x) + abs(end.y - posDir.pos.y) + posDir.rotationCosts(end)
+
+    fun neighboursWithCost(current: PosDir): Set<Pair<PosDir, Int>> {
+        val steps = mutableSetOf<Pair<PosDir, Int>>()
+        for (step in current.direction.neighbourSteps()) {
+            val pos = current.pos + step.pos
+            if (pos !in walls) {
+                steps += PosDir(pos, step.direction) to 1 + if (current.direction == step.direction) 0 else 1000
+            }
+        }
+        return steps
+    }
+
+}
+
+private enum class Direction() {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT;
+
+    fun neighbourSteps(): Set<PosDir> = when (this) {
+        UP -> setOf(PosDir(Pos(-1, 0), LEFT), PosDir(Pos(1, 0), RIGHT), PosDir(Pos(0, -1), UP))
+        DOWN -> setOf(PosDir(Pos(-1, 0), LEFT), PosDir(Pos(1, 0), RIGHT), PosDir(Pos(0, 1), DOWN))
+        RIGHT -> setOf(PosDir(Pos(0, -1), UP), PosDir(Pos(0, 1), DOWN), PosDir(Pos(1, 0), RIGHT))
+        LEFT -> setOf(PosDir(Pos(0, -1), UP), PosDir(Pos(0, 1), DOWN), PosDir(Pos(-1, 0), LEFT))
+    }
+}
+
+
+private fun List<String>.parseInputs(): Map {
+    var start = PosDir(Pos(0, 0), Direction.RIGHT)
+    var end = Pos(0, 0)
+    val walls = mutableSetOf<Pos>()
+    for ((y, row) in withIndex()) {
+        for ((x, ch) in row.withIndex()) {
+            when (ch) {
+                'S' -> start = PosDir(Pos(x, y), Direction.RIGHT)
+                'E' -> end = Pos(x, y)
+                '#' -> walls.add(Pos(x, y))
             }
         }
     }
-    return gardens
-}
-
-private data class Pos(val x: Int, val y: Int)
-
-private data class Area(val type: Char, val pos: Pos) {
-    fun up(): Area = Area(type, Pos(pos.x, pos.y + 1))
-    fun down(): Area = Area(type, Pos(pos.x, pos.y - 1))
-    fun right(): Area = Area(type, Pos(pos.x + 1, pos.y))
-    fun left(): Area = Area(type, Pos(pos.x - 1, pos.y))
-}
-
-private data class Garden(val type: Char, val areas: MutableSet<Area>)
-
-
-private fun List<List<Area>>.findAllNeighbours(area: Area, garden: Garden) {
-    val neighbours = findNeighbours(area)
-    for (neighbour in neighbours) {
-        if (!garden.areas.contains(neighbour)) {
-            garden.areas += neighbour
-            findAllNeighbours(neighbour, garden)
-        }
-    }
-}
-
-
-private fun List<List<Area>>.findNeighbours(area: Area): Set<Area> {
-    val map = this
-    return buildSet {
-        val up = map.get2DValue(area.up())
-        if (up != null) {
-            add(up)
-        }
-        val down = map.get2DValue(area.down())
-        if (down != null) {
-            add(down)
-        }
-        val right = map.get2DValue(area.right())
-        if (right != null) {
-            add(right)
-        }
-        val left = map.get2DValue(area.left())
-        if (left != null) {
-            add(left)
-        }
-    }
-}
-
-private fun List<List<Area>>.get2DValue(area: Area): Area? {
-    var pos = area.pos
-    if (pos.y in indices) {
-        val row = get(pos.y)
-        if (pos.x in row.indices) {
-            val ret = row[pos.x]
-            return if (ret.type == area.type) ret else null
-        }
-    }
-    return null
-}
-
-
-private fun List<String>.parseInputs(): List<List<Area>> {
-    val input = this
-    return buildList {
-        for ((y, row) in input.withIndex()) {
-            add(buildList {
-                for ((x, ch) in row.withIndex()) {
-                    add(Area(ch, Pos(x, y)))
-                }
-            })
-        }
-    }
+    return Map(start, end, walls)
 }
 
 
