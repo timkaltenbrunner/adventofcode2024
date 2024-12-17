f051f6346e9e5092601c20e8a92ec24acf8b8bf6
Tim Kaltenbrunner <tim.kaltenbrunner@gmail.com>
2024-12-17 07:30:28 +0100

    Day 17: Part 1

--- src/main/java/ch/zkb/t632/kotlin/year24/day17/Day17.kt
+++ src/main/java/ch/zkb/t632/kotlin/year24/day17/Day17.kt
@@ -2,44 +2,96 @@ package ch.zkb.t632.kotlin.year24.day17
 
 import ch.zkb.t632.kotlin.check
 import ch.zkb.t632.kotlin.readInput
+import kotlin.math.pow
+import kotlin.math.truncate
 
 fun main() {
+    val testInput_n1 = readInput("2024", "Day17_test4")
+    part1(testInput_n1)
+
+
     val testInput = readInput("2024", "Day17_test")
-    check(part1(testInput), 11048)
+    check(part1(testInput), "4,6,3,5,6,3,5,2,1,0")
+
+    val testInput2 = readInput("2024", "Day17_test2")
+    check(part1(testInput2), "0,1,2")
+
+    val testInput3 = readInput("2024", "Day17_test3")
+    check(part1(testInput3), "4,2,5,6,7,7,7,7,3,1,0")
 
     val input = readInput("2024", "Day17")
     println("Solution of Part1: ${part1(input)}")
 
-    check(part2(testInput), 64)
-    println("Solution of Part2: ${part2(input)}")
+    // println("Solution of Part2: ${part2(input)}")
 }
 
-private fun part1(input: List<String>): Int = input.parseInputs().solve()
+private fun part1(input: List<String>): String = input.parseInputs().solve()
 
-private fun part2(input: List<String>): Int = input.parseInputs().solve()
+private fun part2(input: List<String>): String = input.parseInputs().solve()
 
-private fun Map.solve(): Int = 0
-
-private data class Pos(val x: Int, val y: Int) {
-    operator fun plus(other: Pos): Pos = Pos(x + other.x, y + other.y)
+private fun Computer.solve(): String {
+    var pointer = 0
+    val output = mutableListOf<Int>()
+    while (pointer in inst.indices) {
+        val cur = inst[pointer]
+        when (cur.opcode) {
+            //adv
+            0 -> a = truncate(a / (2.0.pow(cur.operand.combo(this)))).toInt()
+            //bxl
+            1 -> b = b xor cur.operand
+            //bst
+            2 -> b = cur.operand.combo(this) % 8
+            //jnz
+            3 -> {
+                if (a != 0) {
+                    pointer = cur.operand / 2
+                    continue
+                }
+            }
+            //bxc
+            4 -> b = b xor c
+            //out
+            5 -> output += cur.operand.combo(this) % 8
+            //bdv
+            6 -> b = truncate(a / (2.0.pow(cur.operand.combo(this)))).toInt()
+            //cdv
+            7 -> c = truncate(a / (2.0.pow(cur.operand.combo(this)))).toInt()
+        }
+        pointer++
+    }
+    return output.joinToString(",")
 }
 
-private data class Map(var start: Pos, val walls: Set<Pos>) {
+private fun Int.combo(com: Computer): Int {
+    when (this) {
+        in 0..3 -> return this
+        4 -> return com.a
+        5 -> return com.b
+        6 -> return com.c
+        7 -> error("Should not happen")
+    }
+    error("Should not happen")
+}
+
+private data class Inst(val opcode: Int, val operand: Int) {
 
 }
 
-private fun List<String>.parseInputs(): Map {
-    var start = Pos(0, 0)
-    val walls = mutableSetOf<Pos>()
-    for ((y, row) in withIndex()) {
-        for ((x, ch) in row.withIndex()) {
-            when (ch) {
-                'S' -> start = Pos(x, y)
-                '#' -> walls.add(Pos(x, y))
+private data class Computer(var a: Int, var b: Int, var c: Int, val inst: List<Inst>) {
+
+}
+
+private fun List<String>.parseInputs(): Computer {
+    val register = this[0].split(",").map { it.toInt() }
+    val insts = this[1].split(",").map { it.toInt() }
+    val instructions = buildList<Inst> {
+        for (i in insts.indices) {
+            if (i % 2 == 1) {
+                add(Inst(insts[i - 1], insts[i]))
             }
         }
     }
-    return Map(start, walls)
+    return Computer(register[0], register[1], register[2], instructions)
 }
 
 
